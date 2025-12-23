3ce40e98df829f6def80a8063e7a06b24f77a9a7
thetruesorata <244397111+thetruesorata@users.noreply.github.com>
2025-12-23 21:50:17 +0100

    docs: Update HTML title and description

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -38,8 +38,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
