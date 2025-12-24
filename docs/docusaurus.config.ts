f76967a0f49a892b42de693cffdfa089eecf67ec
thetruesorata <244397111+thetruesorata@users.noreply.github.com>
2025-12-24 13:02:04 +0100

    docs: Add `autohotkey` as additional language to Prism settings

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -117,6 +117,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['autohotkey']
     },
   } satisfies Preset.ThemeConfig,
 };
