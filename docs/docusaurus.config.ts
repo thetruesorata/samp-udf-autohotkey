336184321420639df337e63e5d6a4e7224bc3f68
thetruesorata <244397111+thetruesorata@users.noreply.github.com>
2025-12-24 10:23:58 +0100

    docs: Fix edit URLs again

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            `https://github.com/${organizationName}/${projectName}/`,
+            `https://github.com/${organizationName}/${projectName}/tree/main/docs`,
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           admonitions: {
